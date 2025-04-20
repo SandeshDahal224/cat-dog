be2772b1d1320c7efd17670ca5a6a08a0784d602
SandeshDahal224 <sandeshdahal860@gmail.com>
2025-04-20 22:34:45 +0545

    fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { useActionState, useEffect, useState } from "react"
-import cat from "./assets/cat.png"
-import dog from "./assets/dog.png"
+import roti from "./assets/roti.jpeg"
+import hen from "./assets/hen.webp"
+import goat from "./assets/Goat.jpeg"
 function App(){
 
   const [url, setUrl] = useState("");
   const [query, setQuery] = useState("")
    
   const handleUrlChange = () => {
-    if(query == "saroj"){
-      setUrl(cat);
+    if(query == "ishan"){
+      setUrl(hen);
     }
-    else if(query == "sandesh"){
-      setUrl(dog)
+    else if(query == "prashant"){
+      setUrl(goat)
     }
+    else if (query == "rohit")
+    setUrl(roti)
   }
 
  
 return(
-  <div>
-    <h1>Displaying image according to input</h1>
+  <div className=" text-center justify-center h-screen bg-gradient-to-b from-blue-500 to-purple-600">
+    <h1 className="text-4xl font-bold bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent ">Enter Your First Name</h1>
     <input className="border" value={query} onChange={(e)=> setQuery(e.target.value)}/>
-    <button className="border ml-2 cursor-pointer" onClick={handleUrlChange}>Search</button>
-    <img src={url}></img>
+    <button className="border ml-4 cursor-pointer p-2 bg-black text-white" onClick={handleUrlChange}>Search</button>
+    <img src={url} className="mt-4 mx-auto w-64"></img>
   </div>
 )
 }
